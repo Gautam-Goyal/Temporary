570a84731449ab38dc717c14ce3d0b3108078ef6
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-05-17 01:11:39 +0530

    Settings preferences crash

---
+++ EarthQuakeAPI/app/src/main/java/com/example/earthquakeapi/Earthquake.java
@@ -0,0 +1,57 @@
+package com.example.earthquakeapi;
+
+public class Earthquake {
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Location of the earthquake */
+    private String mLocation;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    private String mURL;
+
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     *
+     */
+    public Earthquake(double magnitude, String location,long timeInMilliseconds,String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mURL=url;
+        mTimeInMilliseconds = timeInMilliseconds;
+    }
+
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
+        return mLocation;
+    }
+
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getURL(){
+        return mURL;
+    }
+
+}
